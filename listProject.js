2ee4959b69c1cfb854059a719a8085e422ea1ef6
Evelyn Marie Delph <evdelph@indiana.edu>
2018-05-16 14:28:53 -0400

    Make basic list

--- listProject.js
+++ listProject.js
@@ -1,2 +1,39 @@
 const form = document.querySelector('form#albumForm')
 const list = document.querySelector('#listOutput')
+albumArray = []
+
+const addButton = document.querySelector('#submitType')
+const resetButton = document.querySelector('#resetType')
+const removeButton = document.querySelector('#removeType')
+
+const addToList = function(ev)
+{
+    // Add list item to list
+    ev.preventDefault()
+    const form = ev.target
+
+    const album = renderListItem(form.album.value)
+    const albumArray = addToArray(form.album.value)
+    list.appendChild(album)
+    form.reset()
+    form.album.focus()
+
+}
+
+function renderListItem(album){
+    // Make individual list items
+    const listItem = document.createElement('li')
+    const albumList = document.createElement('ul')
+
+    listItem.textContent = album
+    albumList.appendChild(listItem)
+
+    return albumList
+}
+
+function addToArray(element){
+    albumArray.push(element)
+    return albumArray
+}
+
+form.addEventListener('submit', addToList)
