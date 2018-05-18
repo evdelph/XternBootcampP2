2c9f6f2f1f1fc2190e410c80d90573ca93712c83
Evelyn Marie Delph <evdelph@indiana.edu>
2018-05-18 09:10:55 -0400

    Update fav key on fav click

--- listProject.js
+++ listProject.js
@@ -1,72 +1,74 @@
 const app = {
-    init(selectors) {
-      this.albums = []
-      this.number = 0
-      this.albumlist = document.querySelector(selectors.listSelector)
-      this.template = document.querySelector(selectors.templateSelector)
-  
-      document
-        .querySelector(selectors.formSelector)
-        .addEventListener('submit', ev => {
-          ev.preventDefault()
-          this.handleSubmit(ev)
-        })
-    },
-  
-    renderListItem(album) {
-      const item = this.template.cloneNode(true)
-      item.classList.remove('template')
-      item.dataset.id = album.id
-      item
-        .querySelector('.albumName')
-        .textContent = album.name
+  init(selectors) {
+    this.albums = []
+    this.number = 0
+    this.albumlist = document.querySelector(selectors.listSelector)
+    this.template = document.querySelector(selectors.templateSelector)
 
-      item
-        .querySelector('button#del')
-        .addEventListener('click',this.deleteItem.bind(this,album))
+    document
+      .querySelector(selectors.formSelector)
+      .addEventListener('submit', ev => {
+        ev.preventDefault()
+        this.handleSubmit(ev)
+      })
+  },
 
-      item
-        .querySelector('button#fav')
-        .addEventListener('click',this.favItem.bind(this,album))
-  
-      return item
-    },
-  
-    handleSubmit(ev) {
-      const f = ev.target
-      const album = {
-        id: ++this.number,
-        name: f.albumName.value,
-      }
-  
-      this.albums.unshift(album)
-  
-      const item = this.renderListItem(album)
-      this.albumlist.insertBefore(item, this.albumlist.firstElementChild)
-  
-      f.reset()
-    },
+  renderListItem(album) {
+    const item = this.template.cloneNode(true)
+    item.classList.remove('template')
+    item.dataset.id = album.id
+    item
+      .querySelector('.albumName')
+      .textContent = album.name
 
-    deleteItem(element,ev){
-      const button = ev.target
-      const x = button.closest('.album')
+    item
+      .querySelector('button#del')
+      .addEventListener('click',this.deleteItem.bind(this,album))
 
-      x.remove()
-      const index = this.albums.indexOf(element)
-      this.element.splice(index,1)
-    },
+    item
+      .querySelector('button#fav')
+      .addEventListener('click',this.favItem.bind(this,album))
 
-    favItem(element,ev){
-      const button = ev.target
-      const y = button.closest('.album')
+    return item
+  },
 
-      y.style.borderColor='cornflowerblue'
+  handleSubmit(ev) {
+    const f = ev.target
+    const album = {
+      id: ++this.number,
+      name: f.albumName.value,
+      fav: 'false',
     }
-  }
-  
-  app.init({
-    formSelector: '#albumForm',
-    listSelector: '#albumList',
-    templateSelector: '.album.template',
-  })
 
+    this.albums.unshift(album)
+
+    const item = this.renderListItem(album)
+    this.albumlist.insertBefore(item, this.albumlist.firstElementChild)
+
+    f.reset()
+  },
+
+  deleteItem(element,ev){
+    const button = ev.target
+    const x = button.closest('.album')
+
+    x.remove()
+    const index = this.albums.indexOf(element)
+    this.element.splice(index,1)
+  },
+
+  favItem(element,ev){
+    const button = ev.target
+    const y = button.closest('.album')
+    const index = app.albums.indexOf(element)
+
+    y.style.borderColor='cornflowerblue'
+    app.albums[index]['fav']='true'
+  }
+}
+
+app.init({
+  formSelector: '#albumForm',
+  listSelector: '#albumList',
+  templateSelector: '.album.template',
+})
